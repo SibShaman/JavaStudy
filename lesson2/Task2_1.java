cccf28eeced315ffe43a6d67130238df711ccc8c
Shaman <infografika@inbox.ru>
2022-12-02 18:07:39 +0700

    Begin task 2_3

--- lesson2/Task2_1.java
+++ lesson2/Task2_1.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Task2_1 {
     public static void main(String[] args) throws IOException{
 
--- lesson2/Task2_3.java
+++ lesson2/Task2_3.java
@@ -3,11 +3,49 @@ package lesson2;
 // [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
 // {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
 // {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+
 // Напишите метод, который разберёт её на составные части и, используя
 // StringBuilder создаст массив строк такого вида:
 // Студент Иванов получил 5 по предмету Математика.
 // Студент Петрова получил 4 по предмету Информатика.
 // Студент Краснов получил 5 по предмету Физика.
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task2_3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader("lesson2/rating.json"))){
+            String data;
+            while ((data = br.readLine()) != null){
+                sb.append(data);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        // создание списка словарей
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        // и конечно сам словарь
+        Map<String,String> map = new HashMap<String, String>();
+
+        //Парсим строку, убираем лишние символы и записываем данные в словарь
+        String[] keys = sb.toString().replace("{", "").
+                replace("[","").
+                replace("]", "").
+                replace("\"", "").
+                replaceAll("\\s", "").
+                split("}");
+        for (String data: keys) {
+            System.out.println(data);
+
+        }
+
+//        System.out.println(dictionary);
+    }
 }
