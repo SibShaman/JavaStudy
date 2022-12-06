bbe8a998bd153cf456d586ad030ad204d356671e
Shaman <infografika@inbox.ru>
2022-12-06 23:24:11 +0700

    final task3_1

--- lesson3/Task3_1.java
+++ lesson3/Task3_1.java
@@ -18,10 +18,39 @@ public class Task3_1 {
         System.out.printf("Отсортированный массив %s\n", Arrays.toString(result));
     }
 
+    public static int[] mergeArray(int[] bufOneArray, int[] arrayB) {
+        int n1 = bufOneArray.length, n2 = arrayB.length;
+        int n = n1 + n2, i1 = 0, i2 = 0;
+        int[] resArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i1 == n1) {
+                resArray[i] = arrayB[i2++];
+            } else if (i2 == n2) {
+                resArray[i] = bufOneArray[i1++];
+            } else {
+                if (bufOneArray[i1] < arrayB[i2]) {
+                    resArray[i] = bufOneArray[i1++];
+                } else {
+                    resArray[i] = arrayB[i2++];
+                }
+            }
+        }
+        return resArray;
+    }
+
     public static int[] merge_sort(int[] array){
-        int[] result = Arrays.copyOf(array, array.length);
+        if (array.length<2) return array;
 
+        int[] bufOneArray = new int[array.length/2];
+        System.arraycopy(array, 0 , bufOneArray, 0, array.length/2);
 
-        return result;
+        int[] bufTwoArray = new int[array.length - bufOneArray.length];
+        System.arraycopy(array, array.length / 2, bufTwoArray, 0, array.length - bufOneArray.length);
+
+        bufOneArray = merge_sort(bufOneArray);
+        bufTwoArray = merge_sort(bufTwoArray);
+
+        return mergeArray(bufOneArray, bufTwoArray);
     }
 }
