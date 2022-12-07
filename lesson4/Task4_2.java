f380b6441e110b8a072d01621e5d7a4b8c57bcf7
Shaman <infografika@inbox.ru>
2022-12-07 16:28:18 +0700

    begin task4_2

--- lesson4/Task4_2.java
+++ lesson4/Task4_2.java
@@ -5,5 +5,24 @@ package lesson4;
 //        dequeue() - возвращает первый элемент из очереди и удаляет его,
 //        first() - возвращает первый элемент из очереди, не удаляя
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Task4_2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> originalList = new LinkedList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            originalList.add(i, rand.nextInt(100));
+        }
+        System.out.printf("Исходный список: %s \n", originalList);
+    }
+
+    public static LinkedList<Integer> enqueue(LinkedList<Integer> list, int num){
+
+
+
+        return list;
+    }
 }
