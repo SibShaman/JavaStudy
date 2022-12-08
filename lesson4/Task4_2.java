8e182ea87f3d8ac393af96201c608cd55f41f933
Shaman <infografika@inbox.ru>
2022-12-08 13:38:58 +0700

    final task4_2

--- lesson4/Task4_2.java
+++ lesson4/Task4_2.java
@@ -1,9 +1,6 @@
 package lesson4;
 
 //Реализуйте очередь с помощью LinkedList со следующими методами:
-//        enqueue() - помещает элемент в конец очереди,
-//        dequeue() - возвращает первый элемент из очереди и удаляет его,
-//        first() - возвращает первый элемент из очереди, не удаляя
 
 import java.util.LinkedList;
 import java.util.Random;
@@ -17,12 +14,32 @@ public class Task4_2 {
             originalList.add(i, rand.nextInt(100));
         }
         System.out.printf("Исходный список: %s \n", originalList);
+        System.out.printf("Список с добавлением элемента в конец %s \n", enqueue(originalList, 15));
+
+        System.out.printf("Первый элемент списка %s \n", dequeue(originalList));
+        System.out.printf("Исходный список после удаления первого элемента : %s \n", originalList);
+
+        System.out.printf("Первый элемент списка %s \n", first(originalList));
+        System.out.printf("Исходный список без удаления первого элемента: %s \n", originalList);
     }
 
+    //        enqueue() - помещает элемент в конец очереди,
     public static LinkedList<Integer> enqueue(LinkedList<Integer> list, int num){
-
-
-
+        list.addLast(num);
         return list;
     }
+
+    //        dequeue() - возвращает первый элемент из очереди и удаляет его,
+    public static int dequeue(LinkedList<Integer> list){
+        int num = list.getFirst();
+        // System.out.printf("Первый элемент в списке: %s \n", list.getFirst());
+        list.removeFirst();
+        return num;
+    }
+
+    //        first() - возвращает первый элемент из очереди, не удаляя
+    public static int first(LinkedList<Integer> list){
+        return list.getFirst();
+    }
+
 }
