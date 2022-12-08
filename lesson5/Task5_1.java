8c037a2721653d1ce72c1c3f65571ebd026db72b
Shaman <infografika@inbox.ru>
2022-12-09 05:00:17 +0700

    begin task5_1

--- lesson5/Task5_1.java
+++ lesson5/Task5_1.java
@@ -3,5 +3,16 @@ package lesson5;
 //Реализуйте структуру телефонной книги с помощью HashMap,
 //        учитывая, что один человек может иметь несколько телефонов.
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Task5_1 {
+    public static void main(String[] args) {
+        //список словарей (по задумке, индекс списка это id контакта, словарь соответственно это телефонная книга)
+        LinkedList<HashMap<String, String>> phoneBook = new LinkedList<>();
+
+    }
+
+
 }
